752abbe2353fbd28c0890252578a5e847922458c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-05-09 20:30:31 +0200

    Add cache mechanism

--- index.js
+++ index.js
@@ -3,27 +3,63 @@ const Keyv = require('keyv')
 const booleanPointInPolygon = require('@turf/boolean-point-in-polygon').default
 const {prepareOutput} = require('./lib/result')
 
-async function createGazetteer(dbPath) {
-  const db = new Keyv('sqlite://' + (dbPath || process.env.GAZETTEER_DB_PATH))
-  const items = await db.get('items')
-  const spatialIndex = new Flatbush(items.length)
-  items.forEach(c => spatialIndex.add(...c.bbox))
-  spatialIndex.finish()
+async function createGazetteer(options = {}) {
+  const dbPath = options.dbPath || process.env.GAZETTEER_DB_PATH
+
+  if (!dbPath) {
+    throw new Error('Path to the database is required')
+  }
+
+  const _db = new Keyv('sqlite://' + dbPath)
+  const _cache = new Map()
+  const _cacheEnabled = options.cache === true
+  const _items = await _db.get('items')
+  const _spatialIndex = new Flatbush(_items.length)
+
+  _items.forEach(c => _spatialIndex.add(...c.bbox))
+  _spatialIndex.finish()
+
+  async function _getItem(id) {
+    if (_cacheEnabled && _cache.has(id)) {
+      return _cache.get(id)
+    }
+
+    const item = await _db.get(id)
+
+    if (_cacheEnabled) {
+      _cache.set(id, item)
+    }
+
+    return item
+  }
 
   return {
     async find({lon, lat}) {
-      const indices = spatialIndex.search(lon, lat, lon, lat)
+      const indices = _spatialIndex.search(lon, lat, lon, lat)
       const candidates = await Promise.all(
         indices.map(idx => {
-          const {id} = items[idx]
-          return db.get(id)
+          const {id} = _items[idx]
+          return _getItem(id)
         })
       )
       const point = {type: 'Point', coordinates: [lon, lat]}
       const results = candidates.filter(item => booleanPointInPolygon(point, item.contour))
 
       return results.length > 0 ? prepareOutput(selectBestResult(results)) : null
-    }
+    },
+
+    clearCache() {
+      if (_cacheEnabled) {
+        _cache.clear()
+      }
+    },
+
+    _getItem,
+    _cache,
+    _items,
+    _db,
+    _cacheEnabled,
+    _spatialIndex
   }
 }
 
