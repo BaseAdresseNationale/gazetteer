c74093a8568e67b941cfe264c8a59e16a5ea98a6
Vincent Sagniez <vincent.sagniez@ign.fr>
2023-03-09 13:54:46 +0100

    Maj References pour Cog 2023

--- build/index.js
+++ build/index.js
@@ -11,8 +11,8 @@ const {readShapefile} = require('./read-shapefile')
 
 const gunzip = promisify(zlib.gunzip)
 
-const COMMUNES_URL = 'http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2022/geojson/communes-5m.geojson.gz'
-const COMMUNES_ANCIENNES_URL = 'https://osm13.openstreetmap.fr/~cquest/openfla/export/communes-anciennes-20220101-shp.zip'
+const COMMUNES_URL = 'http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2023/geojson/communes-5m.geojson.gz'
+const COMMUNES_ANCIENNES_URL = 'https://osm13.openstreetmap.fr/~cquest/openfla/export/communes-anciennes-20230101-shp.zip'
 
 function downloadFile(url) {
   return got(url).buffer()
